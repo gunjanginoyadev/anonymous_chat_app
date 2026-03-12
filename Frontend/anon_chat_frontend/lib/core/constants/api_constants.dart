cd996b0f1396ea6c7d69a8bc86e64a77dc36a6a9
gunjanginoyadev <gunjan.ginoya.dev@gmail.com>
2026-03-12 15:56:29 +0530

    Fixed the api urls

--- Frontend/anon_chat_frontend/lib/core/constants/api_constants.dart
+++ Frontend/anon_chat_frontend/lib/core/constants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
-  // static const String baseUrl = 'https://anonymous-chat-app-rvcy.onrender.com';
-  // static const String wsUrl = 'wss://anonymous-chat-app-rvcy.onrender.com';
-  static const String baseUrl = 'http://192.168.1.40:3000';
-  static const String wsUrl = 'ws://192.168.1.40:3000';
+  static const String baseUrl = 'https://anonymous-chat-app-rvcy.onrender.com';
+  static const String wsUrl = 'wss://anonymous-chat-app-rvcy.onrender.com';
+  // static const String baseUrl = 'http://192.168.1.40:3000';
+  // static const String wsUrl = 'ws://192.168.1.40:3000';
 }
