066cc2699b4129f7c06173c219a4a04c439de89f
gunjanginoyadev <gunjan.ginoya.dev@gmail.com>
2026-03-13 18:13:05 +0530

    Fixed env

--- Frontend/anon_chat_frontend/lib/core/constants/env.dart
+++ Frontend/anon_chat_frontend/lib/core/constants/env.dart
@@ -3,6 +3,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class Env {
   Env._();
   static String apiBaseUrl =
-      dotenv.env['API_BASE_URL'] ?? 'http://192.168.1.50:3000';
-  static String wsUrl = dotenv.env['WS_URL'] ?? 'ws://192.168.1.50:3000';
+      dotenv.env['API_BASE_URL'] ?? 'https://anonymous-chat-app-rvcy.onrender.com';
+  static String wsUrl = dotenv.env['WS_URL'] ?? 'wss://anonymous-chat-app-rvcy.onrender.com';
 }
