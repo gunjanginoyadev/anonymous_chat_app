97737f5ac4f42d978b84f1f49e04ce0f7d74897a
gunjanginoyadev <gunjan.ginoya.dev@gmail.com>
2026-03-20 17:35:34 +0530

    Changed the env values

--- Frontend/anon_chat_frontend/lib/core/constants/env.dart
+++ Frontend/anon_chat_frontend/lib/core/constants/env.dart
@@ -1,7 +1,11 @@
 class Env {
   Env._();
-  static String apiBaseUrl = 'https://anonymous-chat-app-rvcy.onrender.com';
-  static String wsUrl = 'wss://anonymous-chat-app-rvcy.onrender.com';
-  // static String apiBaseUrl = 'http://192.168.1.48:4000';
-  // static String wsUrl = 'ws://192.168.1.48:4000';
+
+  // Produnction
+  // static String apiBaseUrl = 'https://anonymous-chat-app-rvcy.onrender.com';
+  // static String wsUrl = 'wss://anonymous-chat-app-rvcy.onrender.com';
+
+  // Development
+  static String apiBaseUrl = 'https://anon-chat-t3qs.onrender.com';
+  static String wsUrl = 'wss://anon-chat-t3qs.onrender.com';
 }
